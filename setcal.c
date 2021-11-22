4f72b592eb3881965a90fed12915c98c9b6c0ed4
Marek Prochazka <m.prochazka2002@gmail.com>
2021-11-22 17:44:59 +0100

    basics
    (cherry picked from commit 24bb16b7996894655b73a07d714326eb0ea0b504)

--- setcal.c
+++ setcal.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+
+
 int main(int argc, char const *argv[])
 {
-    /* code */
+    FILE *file;
+    char *lines[1000];
+
+    file = fopen (argv[1], "r");
+    printf("%s", file[0]);
     return 0;
 }
